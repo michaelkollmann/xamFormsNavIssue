fb6b136863cc4221ae711c0b27e4d82c0825577c
Kollmann, Michael <Michael.Kollmann@presens.de>
2018-10-29 10:38:22 +0100

    Add project files.

---
+++ navigationissue/navigationissue/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using navigationissue.Models;
+
+namespace navigationissue.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
